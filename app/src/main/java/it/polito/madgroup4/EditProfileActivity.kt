49c9903085244acff4ca9dd31ff4d1fce0fe7f76
frusso <fran.russo@reply.it>
2023-04-01 13:45:46 +0200

    handle camera and gallery with startActivityForResult(must be replaced)

--- app/src/main/java/it/polito/madgroup4/EditProfileActivity.kt
+++ app/src/main/java/it/polito/madgroup4/EditProfileActivity.kt
@@ -5,23 +5,36 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import android.Manifest
 import android.app.Activity
+import android.content.ContentValues
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import android.widget.EditText
-import androidx.activity.result.contract.ActivityResultContracts
+import java.io.FileDescriptor
+import java.io.IOException
 
 class EditProfileActivity : AppCompatActivity() {
 
     lateinit var et_name: EditText
 
+    private var imageUri: Uri? = null
+    private val RESULT_LOAD_IMAGE = 123
+    private val IMAGE_CAPTURE_CODE = 654
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        //get edit text
         et_name = findViewById(R.id.name)
+
+        //get camera button
         val imageButton = findViewById<android.widget.ImageButton>(R.id.camera_button)
+
+        //TODO TUTTE QUESTE STRINGHE SE RIUSCIAMO LE METTIAMO COME LABELS CON LE TRADUZIONI
         imageButton.setOnClickListener {
             val options = arrayOf<CharSequence>("Take Photo", "Choose from Gallery")
             val builder = android.app.AlertDialog.Builder(this)
@@ -32,11 +45,7 @@ class EditProfileActivity : AppCompatActivity() {
                         openCameraForResult()
                     }
                     "Choose from Gallery" -> {
-                        val pickPhoto =
-                            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                        if (pickPhoto.resolveActivity(packageManager) != null) startActivity(
-                            pickPhoto
-                        )
+                        openGallery()
                     }
                 }
             }
@@ -44,6 +53,65 @@ class EditProfileActivity : AppCompatActivity() {
         }
     }
 
+    fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE)
+    }
+    fun openCameraForResult() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED) {
+                val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                requestPermissions(permission, 121)
+            } else {
+                openCamera()
+            }
+        } else {
+            println("Version not ok");
+        }
+        true
+    }
+
+    //opens camera so that user can capture image
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val profileImage = findViewById<android.widget.ImageView>(R.id.profile_image)
+        if (requestCode == IMAGE_CAPTURE_CODE && resultCode == Activity.RESULT_OK) {
+            val bitmap = uriToBitmap(imageUri)
+            profileImage.setImageBitmap(bitmap)
+        }
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
+            imageUri = data.data
+            profileImage.setImageURI(imageUri)
+        }
+    }
+
+    private fun uriToBitmap(selectedFileUri: Uri?): Bitmap? {
+        selectedFileUri?.let {
+            try {
+                val parcelFileDescriptor = contentResolver.openFileDescriptor(selectedFileUri, "r")
+                val fileDescriptor: FileDescriptor = parcelFileDescriptor!!.fileDescriptor
+                val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+                parcelFileDescriptor.close()
+                return image
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        return null
+    }
+
+    /*
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
@@ -102,5 +170,7 @@ class EditProfileActivity : AppCompatActivity() {
             }
         }
 
+
+     */
 }
 
