d35f09eba9a8ea83f71e5d0f46e2f3e6e0f3e191
LeonardoVolpini <volpini.leonardo00@gmail.com>
2023-04-20 10:21:44 +0200

    feat: try to import CustomCalendar but I obtain this error "Failed to resolve: com.github.npanigrahy:custom-calendar-view:v1.1"

---
+++ app/src/main/java/it/polito/madgroup4/View/ReservationsActivity.kt
@@ -0,0 +1,9 @@
+package it.polito.madgroup4.View
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+internal class ReservationsActivity : AppCompatActivity() {
+
+}
