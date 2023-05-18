9c1e942098867e593715c8993e69031a82aa01b4
Marco Mele s296538 <s296538@studenti.polito.it>
2023-05-18 20:33:09 +0200

    fix: possiamo scattare una foto e selezionarla della galleria, abbiamo l'uri e la funzione per trasformarlo in bitmap. Ci chiede i permessi ma la prima volta non lancia la fotocamera

--- app/src/main/java/it/polito/madgroup4/view/screens/EditProfile.kt
+++ app/src/main/java/it/polito/madgroup4/view/screens/EditProfile.kt
@@ -1,15 +1,21 @@
 package it.polito.madgroup4.view.screens
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.net.Uri
 import android.provider.MediaStore
-import android.widget.ImageView
+import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
@@ -19,35 +25,45 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.navigation.NavController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.MultiplePermissionsState
+import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.accompanist.permissions.rememberPermissionState
 import it.polito.madgroup4.R
 import it.polito.madgroup4.model.User
 import it.polito.madgroup4.utility.formatDate
 import java.io.File
+import java.io.FileDescriptor
+import java.io.IOException
 import java.text.SimpleDateFormat
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun ProfileScreen(navController: NavController) {
     val user = remember {
@@ -64,7 +80,25 @@ fun ProfileScreen(navController: NavController) {
 
     var editedUser by remember { mutableStateOf(user) }
 
-    val formatter = SimpleDateFormat("dd/MM/yyyy")
+    val openDialog = remember { mutableStateOf(false) }
+
+    var isCameraOpen = remember { mutableStateOf(false) }
+
+    val cameraPermissionState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    )
+
+    val galleryPermissionState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    )
+
 
     val contactItems = listOf(
         listOf(
@@ -92,32 +126,80 @@ fun ProfileScreen(navController: NavController) {
 //        Pair(Icons.Default.Transgender, editedUser.gender)
     )
 
-    var hasImage by remember {
-        mutableStateOf(false)
-    }
+
     var imageUri by remember {
         mutableStateOf<Uri?>(null)
     }
 
+    var editImageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
 
 
-    val imagePicker = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.GetContent(),
-        onResult = { uri ->
-            hasImage = uri != null
-            imageUri = uri
+    val context = LocalContext.current
+
+    val galleryLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult(),
+        onResult = { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                editImageUri = result.data?.data
+//                val imageBitmap = uriToBitmap(editImageUri!!, context)
+//                val rotatedBitmap = rotateBitmap(imageBitmap!!, context, editImageUri)
+
+            }
         }
     )
 
     val cameraLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.TakePicture(),
         onResult = { success ->
-           println(success)
-            println(imageUri)
+            editImageUri = imageUri
+            isCameraOpen.value = false
         }
     )
 
-    val context = LocalContext.current
+    if (openDialog.value) {
+        AlertDialog(onDismissRequest = {
+            openDialog.value = false
+        }, confirmButton = {
+            TextButton(onClick = {
+                if (cameraPermissionState.allPermissionsGranted) {
+                    val values = ContentValues()
+                    values.put(MediaStore.Images.Media.TITLE, "New Picture")
+                    values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+                    imageUri =
+                        context.contentResolver.insert(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            values
+                        )
+                    cameraLauncher.launch(imageUri)
+                    openDialog.value = false
+                } else {
+                    cameraPermissionState.launchMultiplePermissionRequest()
+                }
+            }) {
+                Text("Take a photo")
+            }
+        }, dismissButton = {
+            TextButton(onClick = {
+                if (galleryPermissionState.allPermissionsGranted) {
+                    val galleryIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    galleryLauncher.launch(galleryIntent)
+                    openDialog.value = false
+                } else {
+                    galleryPermissionState.launchMultiplePermissionRequest()
+                }
+            }) {
+                Text("Chose from gallery")
+            }
+        }, title = {
+            Text("Choose an option")
+        }, properties = DialogProperties(
+            dismissOnBackPress = true, dismissOnClickOutside = true
+        )
+        )
+    }
 
     Column(
         Modifier
@@ -131,26 +213,34 @@ fun ProfileScreen(navController: NavController) {
 
 
             Box(Modifier.align(Alignment.CenterHorizontally)) {
-                Image(
-                    painter = painterResource(R.drawable.profile),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(120.dp)
-                        .clip(CircleShape)
-                        .border(1.dp, MaterialTheme.colorScheme.secondary, CircleShape)
-                )
+                if (editImageUri != null) {
+                    println("imageUri not null")
+                    uriToBitmap(editImageUri!!, context)?.let {
+                        Image(
+                            bitmap = it.asImageBitmap(),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(120.dp)
+                                .clip(CircleShape)
+                                .border(1.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+                        )
+                    }
+                } else {
+                    Image(
+                        painter = painterResource(id = R.drawable.profile),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(120.dp)
+                            .clip(CircleShape)
+                            .border(1.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+                    )
+                }
 
                 SmallFloatingActionButton(
                     onClick = {
-                        val values = ContentValues()
-                        values.put(MediaStore.Images.Media.TITLE, "New Picture")
-                        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
-                        imageUri =
-                            context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-/*                        val takePicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                        takePicture.putExtra(MediaStore.EXTRA_OUTPUT, editImageUri)*/
-                        cameraLauncher.launch(imageUri)
+                        openDialog.value = true
                     },
                     modifier = Modifier.align(Alignment.BottomEnd),
                     shape = CircleShape,
@@ -199,86 +289,58 @@ fun ContactItem1(icon: ImageVector?, text: String, label: String, onClick: (Stri
     Spacer(modifier = Modifier.height(10.dp))
 }
 
-
-class ComposeFileProvider : FileProvider(
+@OptIn(ExperimentalPermissionsApi::class)
+private fun openCameraForResult(
+    cameraPermissionState: MultiplePermissionsState,
+    context: Context,
+    imageUri: MutableState<Uri?>,
+    cameraLauncher: ActivityResultLauncher<Uri?>,
+    openDialog: MutableState<Boolean>
 ) {
-    companion object {
-        fun getImageUri(context: Context): Uri {
-            val directory = File(context.cacheDir, "images")
-            directory.mkdirs()
-            val file = File.createTempFile(
-                "selected_image_",
-                ".jpg",
-                directory,
-            )
-            val authority = context.packageName + ".fileprovider"
-            return getUriForFile(
-                context,
-                authority,
-                file,
-            )
-        }
-    }
-}
-
-
-private fun openCameraForResult(context: Context) {
-    //if(takePicture.resolveActivity(packageManager) != null)
-/*    if (!hasCameraPermission()) {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA
-            ),
-            1
-        )*/
-   /* } else {
+    if (cameraPermissionState.allPermissionsGranted) {
         val values = ContentValues()
         values.put(MediaStore.Images.Media.TITLE, "New Picture")
         values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
-        editImageUri =
-            contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-        val takePicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        takePicture.putExtra(MediaStore.EXTRA_OUTPUT, editImageUri)
-        cameraLauncher.launch(takePicture)
-    }*/
-
-
+        imageUri.value =
+            context.contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                values
+            )
+        cameraLauncher.launch(imageUri.value)
+        openDialog.value = false
+    } else {
+        cameraPermissionState.launchMultiplePermissionRequest()
+    }
 }
 
-var editImageUri: Uri? = null
 
-/*
-private var cameraLauncher =
-    rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-
-            val imageBitmap = uriToBitmap(editImageUri!!)
-            val rotatedBitmap = rotateBitmap(imageBitmap!!)
-            imageUri = save_propic_internally(rotatedBitmap!!)
-            val profile_image = findViewById<ImageView>(R.id.profile_image)
-            profile_image.setImageURI(imageUri)
-//                }
-        }
+private fun uriToBitmap(selectedFileUri: Uri, context: Context): Bitmap? {
+    try {
+        val parcelFileDescriptor = context.contentResolver.openFileDescriptor(selectedFileUri, "r")
+        val fileDescriptor: FileDescriptor = parcelFileDescriptor!!.fileDescriptor
+        val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+        parcelFileDescriptor.close()
+        return image
+    } catch (e: IOException) {
+        e.printStackTrace()
     }
-*/
+    return null
+}
+
+@SuppressLint("Range")
+fun rotateBitmap(input: Bitmap, context: Context, editImageUri: Uri): Bitmap? {
+    val orientationColumn =
+        arrayOf(MediaStore.Images.Media.ORIENTATION)
+    val cur: Cursor? =
+        context.contentResolver.query(editImageUri!!, orientationColumn, null, null, null)
+    var orientation = -1
+    if (cur != null && cur.moveToFirst()) {
+        orientation = cur.getInt(cur.getColumnIndex(orientationColumn[0]))
+    }
+    Log.d("tryOrientation", orientation.toString() + "")
+    val rotationMatrix = Matrix()
+    rotationMatrix.setRotate(orientation.toFloat())
+    return Bitmap.createBitmap(input, 0, 0, input.width, input.height, rotationMatrix, true)
+}
 
 
-/*
-
-private fun hasCameraPermission(context: Context): Boolean {
-    return ContextCompat.checkSelfPermission(
-        context,
-        Manifest.permission.READ_EXTERNAL_STORAGE
-    ) == PackageManager.PERMISSION_GRANTED
-            && ContextCompat.checkSelfPermission(
-        context,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE
-    ) == PackageManager.PERMISSION_GRANTED
-            &&
-            ContextCompat.checkSelfPermission(
-                context, Manifest.permission.CAMERA
-            ) == PackageManager.PERMISSION_GRANTED
-}*/
